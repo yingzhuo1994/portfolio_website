3633a16bb53416a5b101089b4747c9ff04319426
yingzhuo <yingzhuo1994@gmail.com>
2023-01-03 12:12:44 -0600

    add  welcome words

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To<br />
+          Ying Zhuo's Website
+        </SectionTitle>
+        <SectionText>
+        This is to show my finished project. If you need my teck skills, please contact me.
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
